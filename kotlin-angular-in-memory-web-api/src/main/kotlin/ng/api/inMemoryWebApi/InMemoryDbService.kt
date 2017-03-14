0dbfc6b69e67250d707ade32bb9e2f05e945b19b
dex01612, Hauke Jaeger <hauke.jaeger@nterra.com>
2017-03-14 17:06:34 +0100

    refactoring

--- kotlin-angular-in-memory-web-api/src/main/kotlin/ng/api/inMemoryWebApi/InMemoryDbService.kt
+++ kotlin-angular-in-memory-web-api/src/main/kotlin/ng/api/inMemoryWebApi/InMemoryDbService.kt
@@ -5,27 +5,5 @@ import kotlin.js.Json
 abstract class InMemoryDbService {
 
     abstract fun createDb(): Json
-
-//    fun createDb(): Json {
-//
-//        val data = createData()
-//        val db: dynamic = js("({})")
-//
-//        data.collections.forEach {
-//
-//            val values: dynamic = js("([])")
-//
-//            it.values.forEach {
-//
-//                values.push(it)
-//            }
-//
-//            db[it.name] = values
-//        }
-//
-//        console.log(db)
-//
-//        return db
-//    }
 }
 
